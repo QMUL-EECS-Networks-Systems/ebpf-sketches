36735687ba9fe6283b5107aeda980fa18ae6cfd3
Sebastiano Miano <mianosebastiano@gmail.com>
2022-12-07 10:20:05 +0000

    Update workflow

--- src/ebpf/common.h
+++ src/ebpf/common.h
@@ -213,7 +213,7 @@ typedef __u16 __attribute__((__may_alias__)) __u16_alias_t;
 typedef __u32 __attribute__((__may_alias__)) __u32_alias_t;
 typedef __u64 __attribute__((__may_alias__)) __u64_alias_t;
 
-static __always_inline void __read_once_size(const volatile void *p, void *res, int size)
+static __always_inline void __read_once_size_custom(const volatile void *p, void *res, int size)
 {
 	switch (size) {
 	case 1: *(__u8_alias_t  *) res = *(volatile __u8_alias_t  *) p; break;
@@ -227,7 +227,7 @@ static __always_inline void __read_once_size(const volatile void *p, void *res, 
 	}
 }
 
-static __always_inline void __write_once_size(volatile void *p, void *res, int size)
+static __always_inline void __write_once_size_custom(volatile void *p, void *res, int size)
 {
 	switch (size) {
 	case 1: *(volatile  __u8_alias_t *) p = *(__u8_alias_t  *) res; break;
@@ -245,7 +245,7 @@ static __always_inline void __write_once_size(volatile void *p, void *res, int s
 ({							\
 	union { typeof(x) __val; char __c[1]; } __u =	\
 		{ .__c = { 0 } };			\
-	__read_once_size(&(x), __u.__c, sizeof(x));	\
+	__read_once_size_custom(&(x), __u.__c, sizeof(x));	\
 	__u.__val;					\
 })
 
@@ -253,7 +253,7 @@ static __always_inline void __write_once_size(volatile void *p, void *res, int s
 ({							\
 	union { typeof(x) __val; char __c[1]; } __u =	\
 		{ .__val = (val) }; 			\
-	__write_once_size(&(x), __u.__c, sizeof(x));	\
+	__write_once_size_custom(&(x), __u.__c, sizeof(x));	\
 	__u.__val;					\
 })
 
